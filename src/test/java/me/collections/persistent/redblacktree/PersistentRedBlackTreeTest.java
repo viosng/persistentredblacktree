63c4ef4d2185de23a4c80dee18e89fe37db0adf1
nickolaysaveliev <nikolay.saveliev@revolut.com>
2017-12-10 04:19:17 +0300

    rotate tests

--- src/test/java/me/collections/persistent/redblacktree/PersistentRedBlackTreeTest.java
+++ src/test/java/me/collections/persistent/redblacktree/PersistentRedBlackTreeTest.java
@@ -12,8 +12,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static me.collections.persistent.redblacktree.Node.Builder.*;
+import static me.collections.persistent.redblacktree.Node.doubleNil;
 import static me.collections.persistent.redblacktree.Node.nil;
 import static me.collections.persistent.redblacktree.PersistentRedBlackTree.balance;
+import static me.collections.persistent.redblacktree.PersistentRedBlackTree.rotate;
 import static me.collections.persistent.redblacktree.Validator.validate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -79,7 +81,7 @@ class PersistentRedBlackTreeTest {
         ThreadLocalRandom random = ThreadLocalRandom.current();
         List<Integer> list = new ArrayList<>();
         PersistentRedBlackTree tree = new PersistentRedBlackTree();
-        for (int i = 0; i < 3000; i++) {
+        for (int i = 0; i < 1000; i++) {
             int v = random.nextInt(10) - 5;
             tree = tree.add(v);
             list.add(v);
@@ -92,4 +94,183 @@ class PersistentRedBlackTreeTest {
             assertEquals(list.stream().sorted().collect(Collectors.toList()), persistentTreeList);
         }
     }
+
+    @Test
+    void should_rotate_red_case_1() {
+        Node node1 = red(1)
+                .left(doubleBlack(2).build())
+                .right(black(3).build())
+                .build();
+        Node expected1 = black(3)
+                .left(red(1)
+                        .left(black(2).build())
+                        .build())
+                .build();
+        assertEquals(balance(expected1), rotate(node1));
+
+        Node node2 = red(1)
+                .left(doubleNil())
+                .right(black(3).build())
+                .build();
+        Node expected2 = black(3)
+                .left(red(1).build())
+                .build();
+        assertEquals(balance(expected2), rotate(node2));
+    }
+
+    @Test
+    void should_rotate_red_case_2() {
+        Node node1 = red(1)
+                .left(black(3).build())
+                .right(doubleBlack(2).build())
+                .build();
+        Node expected1 = black(3)
+                .right(red(1)
+                        .right(black(2).build())
+                        .build())
+                .build();
+        assertEquals(balance(expected1), rotate(node1));
+
+        Node node2 = red(1)
+                .left(black(3).build())
+                .right(doubleNil())
+                .build();
+        Node expected2 = black(3)
+                .right(red(1).build())
+                .build();
+        assertEquals(balance(expected2), rotate(node2));
+    }
+
+    @Test
+    void should_rotate_black_case_1() {
+        Node node1 = black(1)
+                .left(doubleBlack(2).build())
+                .right(black(3).build())
+                .build();
+        Node expected1 = doubleBlack(3)
+                .left(red(1)
+                        .left(black(2).build())
+                        .build())
+                .build();
+        assertEquals(balance(expected1), rotate(node1));
+
+        Node node2 = black(1)
+                .left(doubleNil())
+                .right(black(3).build())
+                .build();
+        Node expected2 = doubleBlack(3)
+                .left(red(1).build())
+                .build();
+        assertEquals(balance(expected2), rotate(node2));
+    }
+
+    @Test
+    void should_rotate_black_case_2() {
+        Node node1 = black(1)
+                .left(black(3).build())
+                .right(doubleBlack(2).build())
+                .build();
+        Node expected1 = doubleBlack(3)
+                .right(red(1)
+                        .right(black(2).build())
+                        .build())
+                .build();
+        assertEquals(balance(expected1), rotate(node1));
+
+        Node node2 = black(1)
+                .left(black(3).build())
+                .right(doubleNil())
+                .build();
+        Node expected2 = doubleBlack(3)
+                .right(red(1).build())
+                .build();
+        assertEquals(balance(expected2), rotate(node2));
+    }
+
+    @Test
+    void should_rotate_black_case_3() {
+        Node node1 = black(1)
+                .left(doubleBlack(2).build())
+                .right(red(3)
+                        .left(black(4).build())
+                        .build())
+                .build();
+        Node expected1 = black(3)
+                .left(balance(black(4)
+                                .left(red(1)
+                                        .left(black(2).build())
+                                        .build())
+                                .build()
+                        )
+                )
+                .build();
+        assertEquals(expected1, rotate(node1));
+
+        Node node2 = black(1)
+                .left(doubleNil())
+                .right(red(3)
+                        .left(black(4).build())
+                        .build())
+                .build();
+        Node expected2 = black(3)
+                .left(balance(black(4)
+                                .left(red(1).build())
+                                .build()
+                        )
+                )
+                .build();
+        assertEquals(expected2, rotate(node2));
+    }
+
+    @Test
+    void should_rotate_black_case_4() {
+        Node node1 = black(1)
+                .left(red(3)
+                        .right(black(4).build())
+                        .build())
+                .right(doubleBlack(2).build())
+                .build();
+        Node expected1 = black(3)
+                .right(balance(black(4)
+                                .right(red(1)
+                                        .right(black(2).build())
+                                        .build())
+                                .build()
+                        )
+                )
+                .build();
+        assertEquals(expected1, rotate(node1));
+
+        Node node2 = black(1)
+                .left(red(3)
+                        .right(black(4).build())
+                        .build())
+                .right(doubleNil())
+                .build();
+        Node expected2 = black(3)
+                .right(balance(black(4)
+                                .right(red(1).build())
+                                .build()
+                        )
+                )
+                .build();
+        assertEquals(expected2, rotate(node2));
+    }
+
+    private static Stream<Node> createNotRotateTests() {
+        return Stream.of(
+                nil(),
+                red(3).build(),
+                red(2).right(red(3).build()).build(),
+                black(2).left(red(3).build()).build(),
+                black(2).left(doubleBlack(3).build()).build()
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createNotRotateTests")
+    void should_not_rotate(Node x) {
+        assertEquals(x, rotate(x));
+    }
+
 }
